4970a1ee228b62e707d34166f55346192c7d2598
Yuri <isyuricunha@duck.com>
2026-08-07 16:28:19 -0300

    chore: centralize direct hotkey defaults

---
+++ core/hotkey_defaults.py
@@ -0,0 +1,13 @@
+"""Default hotkey configuration shared by the application and settings UI."""
+
+DIRECT_ACTION_HOTKEYS = {
+    "grammar_check": "<ctrl>+<alt>+g",
+    "rewrite": "<ctrl>+<alt>+r",
+    "translate": "<ctrl>+<alt>+t",
+}
+
+DIRECT_ACTION_LABELS = {
+    "grammar_check": "Grammar Check",
+    "rewrite": "Rewrite",
+    "translate": "Translate",
+}
